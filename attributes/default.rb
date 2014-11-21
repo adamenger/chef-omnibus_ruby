6416d5e14fbf16e3befc760128bd4b5e01db4190
Adam Enger <adamenger@gmail.com>
2014-11-21 09:08:34 -0600

    adding checksum attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,7 @@ default['omnibus_ruby']['version'] = '2.1.2'
 # You can host your package anywhere, but this should be the subdirectory in which the package lives.
 # e.g: http://your-site.com/downloads
 default['omnibus_ruby']['download_url'] = "https://s3.amazonaws.com/your-bucket"
+default['omnibus_ruby']['package_checksum'] = "your-checksum"
 
 # path where we want to download the ruby package to, no trailing slash please!
 default['omnibus_ruby']['download_path'] = "/tmp"
